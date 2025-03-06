5060676294770230846c96ff69354f60c2cc6a1d
ardiansetya <ardiansetya129@gmail.com>
2025-03-06 20:07:32 +0700

    create head title meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,16 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={GeistSans.className}>
       <ClerkProvider>
+        <Head>
+          <title>Twitter Clone</title>
+          <meta name="description" content="Twitter Clone by Michelle" />
+        </Head>
         <Component {...pageProps} />
       </ClerkProvider>
     </div>
