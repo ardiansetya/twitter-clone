1834edb31c871a78cc8bc2b1de7ff666d81ba8aa
ardiansetya <ardiansetya129@gmail.com>
2025-03-08 19:41:15 +0700

    update textarea input size

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,17 +1,41 @@
 import { UserButton, useUser } from "@clerk/nextjs";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
 
+function updateTextAreaSize (textArea: HTMLTextAreaElement) {
+  if (textArea == null) return
+  textArea.style.height = "0"
+  textArea.style.height = `${textArea.scrollHeight}px`
+
+}
 const NewTweetForm = () => {
-    const {user} = useUser()
+  const { user } = useUser();
+
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  useEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  
+   
+  }, [inputValue])
+  
+
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        <ProfileImage src={user?.imageUrl}/>
-        <textarea placeholder="What's happening" className="p-4 outline-none resize-none text-lg overflow-hidden flex-grow" />
+        <ProfileImage src={user?.imageUrl} />
+        <textarea
+          style={{ height: 0 }}
+          value={inputValue}
+          ref={textAreaRef}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="What's happening"
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+        />
       </div>
-      <Button className="self-end" >Tweet</Button>
+      <Button className="self-end">Tweet</Button>
     </form>
   );
 };
